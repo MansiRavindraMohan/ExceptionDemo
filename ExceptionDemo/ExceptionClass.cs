5847fcc7096ab4b14aa1c42a636bf824a0f8d79a
MansiRavindraMohan <manasimohan29@gmail.com>
2023-05-18 09:54:19 +0530

    Add: Program for exception demo

---
+++ ExceptionDemo/ExceptionClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionDemo
+{
+    public class ExceptionClass
+    {
+        public void ExceptionMethod()
+        {
+            var filename = "words.txt";
+            if(File.Exists(filename))
+            {
+                var content = File.ReadAllText(filename);
+                Console.WriteLine(content);
+            }
+            try
+            {
+                var contentOne = File.ReadAllText(filename);
+                Console.WriteLine(contentOne);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("failed to read file");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ ExceptionDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ExceptionClass exception=new ExceptionClass();
+            exception.ExceptionMethod();
+            Console.ReadLine();
+        }
+    }
+}
